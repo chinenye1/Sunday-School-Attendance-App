2bc851dd90f0bec27458efc2544a3b2ec10fc3ec
Chinenye Oluka <olukachinenye@yahoo.com>
2020-11-24 17:30:39 -0800

    P4 - Task2 completed + Checkstyle + comments

--- src/main/ui/HandleAudio.java
+++ src/main/ui/HandleAudio.java
@@ -8,13 +8,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-/** This class deals with playing audio in the program
+/**
+ * This class deals with playing audio in the program
  * This class was modeled after the code in this video: https://www.youtube.com/watch?v=3q4f6I5zi2w
  */
 public class HandleAudio {
+
+    // EFFECTS: creates an instance of HandleAudio
     public HandleAudio() {
     }
 
+    // REQUIRES: filepath should be a locally-saved location
+    // EFFECTS: Takes is the location of audio to played and plays it
     public void playAudio(String filepath) {
         InputStream music;
         try {
@@ -22,7 +27,7 @@ public class HandleAudio {
             AudioStream audio = new AudioStream(music);
             AudioPlayer.player.start(audio);
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null,"There was an error Playing Audio");
+            JOptionPane.showMessageDialog(null, "There was an error Playing Audio");
         }
 
     }
